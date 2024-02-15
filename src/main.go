9d4a7fec139605b7c18977f3fa58729a8ca1c218
Nicholas Jackson <jackson.nic@gmail.com>
2024-02-15 09:11:58 +0000

    Update main.go

--- src/main.go
+++ src/main.go
@@ -12,5 +12,5 @@ func main() {
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from HashiTalks 2024 v0.2")
+	fmt.Fprintf(w, "Hello from HashiTalks 2024 v0.3")
 }
